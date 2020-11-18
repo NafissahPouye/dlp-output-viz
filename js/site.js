01040cbd73506dffb3be32bd63bb8ee24b04c235
Nafissah Pouye <Nafissah Pouye>
2020-11-18 15:59:58 +0000

    initial commit

--- js/site.js
+++ js/site.js
@@ -2,11 +2,9 @@ function generatingComponent(vardata){
 var reqs = dc.rowChart('#rd') ;
 var use = dc.rowChart('#pp') ;
 var dataTab1 = dc.dataTable('#dataTable1');
-//  var orgs = dc.rowChart('#org') ;
-//var Rstate = dc.pieChart ('#State') ;
 var Rstatus = dc.rowChart ('#org') ;
-  //var Rtype = dc.pieChart ('#type') ;
- var colors = ['#FAE61E','#03a9f4','#E67800','#C80000','#E6E6FA', '#023858', '#a6bddb','#3690c0'] ;
+ 
+var colors = ['#FAE61E','#03a9f4','#E67800','#C80000','#E6E6FA', '#023858', '#a6bddb','#3690c0'] ;
 
 var cf = crossfilter(vardata);
 var all = cf.groupAll();
@@ -14,19 +12,14 @@ var colors = ['#2C5197','#0B0B61'];
     
 var statusDim = cf.dimension(function(d) {return d.likelihood});
 var statusGroup = statusDim.group();
-//var RtypeDim = cf.dimension(function(d) { return d.contact});
-//var RtypeGroup = RtypeDim.group();
-//var statusDim = cf.dimension(function(d) {return d.feedback});
+
 var reqDim = cf.dimension(function(d) {return d.infoType});
 var reqGroup = reqDim.group();
 
 var useDim = cf.dimension(function(d) {return d.quote});
 var useGroup = useDim.group();
 
-/*var orgDim = cf.dimension(function(d) {return d.organisation});
-var orgGroup = orgDim.group();
-*/
-//dataTab1
+
      dataTab1
         .size(600)
         .dimension(statusDim)
@@ -46,7 +39,7 @@ var orgGroup = orgDim.group();
                 return d.quote;
                 }
 ])
-//pie charts
+
    
 Rstatus.width(400).height(400)
         .dimension(statusDim)
@@ -59,7 +52,7 @@ Rstatus.width(400).height(400)
             .colorAccessor(function(d, i){return 0;})
             .gap(5);
     
-//Rstatus.ordinalColors(['#C0D7EB','#007CE1']);
+
     
 use.width(400).height(400)
         .dimension(useDim)
@@ -67,7 +60,7 @@ use.width(400).height(400)
         .data(function(group) {
                 return group.top(4);
             }); 
-   // Rtype.ordinalColors(['#C0D7EB','#007CE1', '#E6E7E8']);
+   
 // rowCharts
   reqs.width(500).height(500)
             .dimension(reqDim)
@@ -92,21 +85,7 @@ use.width(400).height(400)
             .colorAccessor(function(d, i){return 0;});
     
 
-// the option text can be set via the title() function
-// by default the option text is '`key`: `value`'
-         
-  /* orgs.width(500)
-     .height(500)
-            .dimension(orgDim)
-            .group(orgGroup)
-            .elasticX(true)
-            .data(function(group) {
-                return group.top(20);
-            })
-            .colors('#FF8C00')
-            .colorAccessor(function(d, i){return 0;})
-            .gap(5);         
-                */
+
 
   dc.dataCount('count-info')
 
@@ -124,32 +103,16 @@ var dataCall = $.ajax({
 
     type: 'GET',
 
-    url: 'data/data1.json',
+    url: 'data/data.json',
 
     dataType: 'json',
 
 });
 
-var geomCall = $.ajax({
 
-    type: 'GET',
 
-    url: 'data/cmr.geojson',
+$.when(dataCall).then(function(dataArgs){
 
-    dataType: 'json',
-
-});
-
-$.when(dataCall, geomCall).then(function(dataArgs, geomArgs){
-
-    var geom = geomArgs[0];
-
-    geom.features.forEach(function(e){
-
-        e.properties['NAME'] = String(e.properties['NAME']);
-
-    });
-
-    generatingComponent(dataArgs[0],geom);
+       generatingComponent(dataArgs);
 
 });
