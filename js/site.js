886bdcfa63107c59cc04239a4a7784f7f86a069e
Nafissah Pouye <Nafissah Pouye>
2020-11-19 16:23:03 +0000

    add key figures

--- js/site.js
+++ js/site.js
@@ -1,52 +1,50 @@
-function generatingComponent(vardata){
-var reqs = dc.rowChart('#rd') ;
-var use = dc.rowChart('#pp') ;
-var dataTab1 = dc.dataTable('#dataTable1');
-var Rstatus = dc.rowChart ('#org') ;
- 
+function generatingCharts(vardata){
+var infotype = dc.rowChart('#InfoType') ;
+var quote = dc.rowChart('#Quote') ;
+var dataTab = dc.dataTable('#dataTable');
+var likelihood = dc.rowChart ('#Likelihood') ;
+var numberDisplayQuote = dc.numberDisplay("#number-display-quote"); 
+var numberDisplayInfotype = dc.numberDisplay("#number-display-infotype"); 
 var colors = ['#FAE61E','#03a9f4','#E67800','#C80000','#E6E6FA', '#023858', '#a6bddb','#3690c0'] ;
 
 var cf = crossfilter(vardata);
 var all = cf.groupAll();
 var colors = ['#2C5197','#0B0B61'];
     
-var statusDim = cf.dimension(function(d) {return d.likelihood});
-var statusGroup = statusDim.group();
+var likelihoodDimension = cf.dimension(function(d) {return d.likelihood});
+var likelihoodGroup = likelihoodDimension.group();
 
-var reqDim = cf.dimension(function(d) {return d.infoType});
-var reqGroup = reqDim.group();
+var infotypeDimension = cf.dimension(function(d) {return d.infoType});
+var infoTypeGroup = infotypeDimension.group();
 
-var useDim = cf.dimension(function(d) {return d.quote});
-var useGroup = useDim.group();
+var quoteDimension = cf.dimension(function(d) {return d.quote});
+var quoteGroup = quoteDimension.group();
 
+var quoteDimensionUnique = quoteDimension.group().reduceCount();
 
-     dataTab1
+     dataTab
         .size(600)
-        .dimension(statusDim)
+        .dimension(quoteDimension)
         .group(function (d) {
-            return d.statusGroup;
+            return d.quoteGroup;
         })
         .columns([
                     function (d) {
                 return d.infoType;
                 },
-                    function (d) {
-                return d.likelihood;
-                },
-                 
-                             
+                           
                 function (d) {
                 return d.quote;
                 }
 ])
 
    
-Rstatus.width(400).height(400)
-        .dimension(statusDim)
-        .group(statusGroup)
+likelihood.width(500).height(500)
+        .dimension(likelihoodDimension)
+        .group(likelihoodGroup)
         .elasticX(true)
              .data(function(group) {
-                return group.top(25);
+                return group.top(20);
             })
             .colors('#3690c0')
             .colorAccessor(function(d, i){return 0;})
@@ -54,46 +52,44 @@ Rstatus.width(400).height(400)
     
 
     
-use.width(400).height(400)
-        .dimension(useDim)
-        .group(reqGroup)
-        .data(function(group) {
-                return group.top(4);
-            }); 
-   
+
 // rowCharts
-  reqs.width(500).height(500)
-            .dimension(reqDim)
-            .group(reqGroup)
+  infotype.width(500).height(500)
+            .dimension(infotypeDimension)
+            .group(infoTypeGroup)
              .elasticX(true)
              .data(function(group) {
-                return group.top(25);
+                return group.top(50);
             })
             .colors('#3690c0')
             .colorAccessor(function(d, i){return 0;})
             .gap(5);
     
-  use.width(500)
+  quote.width(500)
      .height(500)
-            .dimension(reqDim)
-            .group(useGroup)
+            .dimension(quoteDimension)
+            .group(quoteGroup)
             .elasticX(true)
             .data(function(group) {
-                return group.top(3);
+                return group.top(5);
             })
             .colors('#3690c0')
             .colorAccessor(function(d, i){return 0;});
     
+// Number Display
+   
+numberDisplayQuote
+   .group(quoteDimensionUnique);
+    
+numberDisplayInfotype
+   .group(infoTypeGroup);
 
+ dc.dataCount('number-display-infotype')
 
+    .dimension(quoteDimensionUnique)
 
-  dc.dataCount('count-info')
+    .group(quoteGroup);
 
-    .dimension(cf)
-
-    .group(all);
-
-  
   dc.renderAll();
      
 
@@ -113,6 +109,6 @@ var dataCall = $.ajax({
 
 $.when(dataCall).then(function(dataArgs){
 
-       generatingComponent(dataArgs);
+       generatingCharts(dataArgs);
 
 });
