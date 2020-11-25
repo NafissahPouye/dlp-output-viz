da7132d00153209af3d7456dc6c66a04a8aabfe8
Nafissah Pouye <Nafissah Pouye>
2020-11-25 13:58:25 +0000

    update figures

--- js/site.js
+++ js/site.js
@@ -5,22 +5,67 @@ var dataTab = dc.dataTable('#dataTable');
 var likelihood = dc.rowChart ('#Likelihood') ;
 var numberDisplayQuote = dc.numberDisplay("#number-display-quote"); 
 var numberDisplayInfotype = dc.numberDisplay("#number-display-infotype"); 
-var colors = ['#FAE61E','#03a9f4','#E67800','#C80000','#E6E6FA', '#023858', '#a6bddb','#3690c0'] ;
+
 
 var cf = crossfilter(vardata);
 var all = cf.groupAll();
 var colors = ['#2C5197','#0B0B61'];
     
 var likelihoodDimension = cf.dimension(function(d) {return d.likelihood});
-var likelihoodGroup = likelihoodDimension.group();
+var likelihoodGroup = likelihoodDimension.group().reduceCount();
 
 var infotypeDimension = cf.dimension(function(d) {return d.infoType});
 var infoTypeGroup = infotypeDimension.group();
 
 var quoteDimension = cf.dimension(function(d) {return d.quote});
 var quoteGroup = quoteDimension.group();
+//var quoteGoupTab =  quoteDimension.group().reduceSum(function (d) [return d.quote]) 
+// display uniques quotes
+var quoteGroupU = quoteDimension.groupAll().reduce(
+  function (p, v) { //add
+    if(p[v.quote]) {
+      p[v.quote]++;
+    } else {
+      p[v.quote] = 1;
+    }
+    return p;
+  },
+  function (p, v) { //remove
+    p[v.quote]--;
+    if(p[v.quote] === 0) {
+      delete p[v.quote];
+    }
+    return p;
+  },
+  function () { //init
+    //initial p 
+    return {};
+  }
+);
 
-var quoteDimensionUnique = quoteDimension.group().reduceCount();
+// display unique infotype
+var infotypeGroupU = infotypeDimension.groupAll().reduce(
+  function (p, v) { //add
+    if(p[v.infoType]) {
+      p[v.infoType]++;
+    } else {
+      p[v.infoType] = 1;
+    }
+    return p;
+  },
+  function (p, v) { //remove
+    p[v.infoType]--;
+    if(p[v.infoType] === 0) {
+      delete p[v.infoType];
+    }
+    return p;
+  },
+  function () { //init
+    //initial p
+    return {};
+  }
+);   
+    
 
      dataTab
         .size(600)
@@ -38,57 +83,62 @@ var quoteDimensionUnique = quoteDimension.group().reduceCount();
                 }
 ])
 
-   
+
 likelihood.width(500).height(500)
         .dimension(likelihoodDimension)
         .group(likelihoodGroup)
+        
         .elasticX(true)
-             .data(function(group) {
-                return group.top(20);
-            })
-            .colors('#3690c0')
-            .colorAccessor(function(d, i){return 0;})
-            .gap(5);
-    
-
-    
+        .x(d3.scale.linear())
+         .gap(5)
+             /*.data(function(group) {
+                return group.top(10);
+            })*/
+            .colors('#007CE0')
+            //.colorAccessor(function(d, i){return 0;}) 
+            .ordering(function(d){ return -d.value; });
 
 // rowCharts
   infotype.width(500).height(500)
             .dimension(infotypeDimension)
             .group(infoTypeGroup)
-             .elasticX(true)
-             .data(function(group) {
+         .elasticX(true)
+            /* .data(function(group) {
                 return group.top(50);
-            })
-            .colors('#3690c0')
-            .colorAccessor(function(d, i){return 0;})
-            .gap(5);
+            })*/
+            .colors('#007CE0')
+            
+            .ordering(function(d){ return d.value; });
     
+  
   quote.width(500)
      .height(500)
             .dimension(quoteDimension)
             .group(quoteGroup)
             .elasticX(true)
             .data(function(group) {
-                return group.top(5);
+                return group.top(10);
             })
-            .colors('#3690c0')
-            .colorAccessor(function(d, i){return 0;});
+            .colors('#007CE0')
+            
+            .ordering(function(d){ return d.value; });
     
 // Number Display
-   
+
+
 numberDisplayQuote
-   .group(quoteDimensionUnique);
-    
+   .group(quoteGroupU)
+   .valueAccessor(
+    function (d) { return Object.keys(d).length; }
+  );
+   
 numberDisplayInfotype
-   .group(infoTypeGroup);
+   .group(infotypeGroupU)
+   .valueAccessor(
+    function (d) { return Object.keys(d).length; }
+  );
 
- dc.dataCount('number-display-infotype')
 
-    .dimension(quoteDimensionUnique)
-
-    .group(quoteGroup);
 
   dc.renderAll();
      
